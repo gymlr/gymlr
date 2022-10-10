af55e0da41e2a0f81f43caa9ec7018235330af5f
Koni vKxni <eventbeate@gmail.com>
2022-10-10 18:37:00 +0200

    redirect url

--- api/gymlr/lib/gymlr_web/controllers/gymlr_controller.ex
+++ api/gymlr/lib/gymlr_web/controllers/gymlr_controller.ex
@@ -1,7 +1,7 @@
 defmodule GymlrWeb.MainController do
   use GymlrWeb, :controller
 
-  @github_url "https://github.com/gymlr/gymlr"
+  @github_url "https://github.com/gymlr/gymlr/tree/master/api/gymlr#usage"
 
   def index(conn, _params) do
     path = @github_url
