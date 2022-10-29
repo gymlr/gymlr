40654ee8ac057e0286d4378cbd6ad9223058350c
vKxni <74613145+vKxni@users.noreply.github.com>
2022-10-29 20:55:31 +0200

    docs

--- api/gymlr/lib/gymlr_web/controllers/gymlr_controller.ex
+++ api/gymlr/lib/gymlr_web/controllers/gymlr_controller.ex
@@ -1,5 +1,10 @@
 defmodule GymlrWeb.MainController do
   use GymlrWeb, :controller
+  
+  @moduledoc """
+  Triggered once a user enters the basic url, without endpoint.
+  Automatically redirects to the GitHub usage part. 
+  """
 
   @github_url "https://github.com/gymlr/gymlr/tree/master/api/gymlr#usage"
 
